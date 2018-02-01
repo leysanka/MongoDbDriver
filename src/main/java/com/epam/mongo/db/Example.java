39be242d9d9fac921875b62f63552462ee9a2081
Alesia Kastsiuchenka <Alesia_Kastsiuchenka@epam.com>
2018-02-02 02:31:20 +0300

    Simple Db connection established

--- src/main/java/com/epam/mongo/db/Example.java
+++ src/main/java/com/epam/mongo/db/Example.java
@@ -1,5 +1,26 @@
 package com.epam.mongo.db;
 
+import com.mongodb.Block;
+import com.mongodb.DB;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoIterable;
+
+import java.util.function.Consumer;
+
 public class Example {
+
+    public static void main(String[] args) {
+
+        MongoClient mongoClient = new MongoClient("localhost", 27017);
+        MongoDatabase db = mongoClient.getDatabase("myFirstMongo");
+        mongoClient.listDatabaseNames().forEach((Block<? super String>) System.out::println);
+
+        db.createCollection("customers");
+        db.listCollectionNames().forEach((Block<? super String>) c -> System.out.println(c));
+
+
+
+    }
 
 }
